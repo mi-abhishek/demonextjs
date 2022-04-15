b9f152222091fc9eadfc5b9dc88cc95691d10492
Abhishek Goswami <abhishek.goswami@mindinventory.com>
2022-04-15 14:53:31 +0530

    Sitemap added and remove 301 redirect for about us page

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects(){
-    return [
-      {
-        source: '/about-us',
-        destination: '/',
-        permanent: true
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
